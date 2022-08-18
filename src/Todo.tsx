8ba2da2a1f74331418818cb66d9d8709225188fa
Sayuri Hamada <110365626+sayuri-hamada@users.noreply.github.com>
2022-08-18 17:36:09 +0900

    型定義を効率的に管理しよう

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,11 +1,11 @@
-type TodoType = {
-  userId: number;
-  title: string;
-  // propsとして受け取らなくてもいい場合はプロパティ名の後ろに？をつける
-  completed?: boolean;
-};
+import { TodoType } from "./types/Todo";
 
-export const Todo = (props: TodoType) => {
+export const Todo = (
+  //  必要な型のみを抽出する場合はPickを使用する
+  //  props: Pick<TodoType, "userId" | "title" | "completed">
+  // 不必要な型を排除して使用する場合はOmitを使用する
+  props: Omit<TodoType, "id">
+) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark} ${title}（ユーザー：${userId}）`}</p>;
