0aa3e7fde802ffe04b16d3fcc29ab0c5741327d1
Sayuri Hamada <110365626+sayuri-hamada@users.noreply.github.com>
2022-08-18 17:26:53 +0900

    propsに型を定義しよう

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,12 @@ export default function App() {
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
-        <Todo key={todo.id} title={todo.title} userId={todo.userId} />
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
       ))}
     </div>
   );
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,4 +1,12 @@
-export const Todo = (props) => {
-  const { title, userId } = props;
-  return <p>{`${title}（ユーザー：${userId}）`}</p>;
+type TodoType = {
+  userId: number;
+  title: string;
+  // propsとして受け取らなくてもいい場合はプロパティ名の後ろに？をつける
+  completed?: boolean;
+};
+
+export const Todo = (props: TodoType) => {
+  const { title, userId, completed = false } = props;
+  const completeMark = completed ? "[完]" : "[未]";
+  return <p>{`${completeMark} ${title}（ユーザー：${userId}）`}</p>;
 };
